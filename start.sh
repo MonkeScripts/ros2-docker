866f699fde8f71fed2d79a15258fe3b647216f19
ltzehan <ltzehan13@gmail.com>
2023-12-11 02:46:35 -0800

    feat: X11 forwarding

--- start.sh
+++ start.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
-docker rm ros2 && docker build -t ros2 .
+xhost +local:docker
+docker rm ros2 || docker build -t ros2 .
 docker run -it \
   --name ros2 \
+  --runtime nvidia \
+  --network host \
   --mount type=bind,source=/home/jellyfish2/ros2_ws/,target=/home/bb/ros2_ws/ \
   --mount type=bind,source=/mnt/ros2-ccache,target=/home/bb/.ccache \
   --mount type=bind,source=/dev/,target=/dev/ \
+  -e DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v "$HOME/.Xauthority:/home/bb/.Xauthority:rw" \
   ros2
+xhost -
