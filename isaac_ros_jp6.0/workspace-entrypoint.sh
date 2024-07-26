f3590cdca9c6aebb2050770ed0c19454a62c6ff9
SamuelFoo <fooenzesamuel@gmail.com>
2024-07-27 04:19:31 +0800

    Add aliases and source on entry

--- isaac_ros_jp6.0/workspace-entrypoint.sh
+++ isaac_ros_jp6.0/workspace-entrypoint.sh
@@ -15,19 +15,25 @@ source /opt/ros/${ROS_DISTRO}/setup.bash
 # Restart udev daemon
 sudo service udev restart
 
-# Add foxglove-bridge alias
+# Add aliases
 echo 'alias foxglove-bridge="ros2 launch foxglove_bridge foxglove_bridge_launch.xml port:=8765"' >> ~/.bash_aliases
+echo 'alias start-px4-agent="export FASTRTPS_DEFAULT_PROFILES_FILE=/usr/local/share/middleware_profiles/rtps_udp_profile.xml && \
+    export RMW_FASTRTPS_USE_QOS_FROM_XML=1 && \
+    MicroXRCEAgent udp4 -p 8888"' >> ~/.bash_aliases
+echo 'alias udp4-dds-config="export FASTRTPS_DEFAULT_PROFILES_FILE=/usr/local/share/middleware_profiles/rtps_udp_profile.xml"' >> ~/.bash_aliases
 
 if [ -d /workspaces/isaac_ros-dev ]; then 
     cd /workspaces/isaac_ros-dev ; 
     echo "rosdep updating....." ; 
     rosdep install --from-paths src -y --ignore-src ; 
+    source install/setup.bash
 fi
 
 if [ -d /workspaces/drone ]; then 
     cd /workspaces/drone ; 
     echo "rosdep updating....." ; 
     rosdep install --from-paths src -y --ignore-src ; 
+    source install/setup.bash
 fi
 
 $@
